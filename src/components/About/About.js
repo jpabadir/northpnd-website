1d8a41f6c56348552ca33b921185e180d8fc4212
Jean-Philippe Abadir <jpabadir@gmail.com>
2021-05-24 18:57:10 -0400

    Fix about video dimensions

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,21 +14,21 @@ function About() {
           <div style={{ fontSize: '20px' }}>North P&amp;D is a newly-established company, for which I do the entirety of the work.</div>
           <div style={{ fontSize: '20px' }}>
             <br />
-            Before starting this company, I worked on a variety of web-projects, I interned as a software engineer at Amazon Web Services and RBC, and I also
-            launched an iOS app called Habit One, which you can download on the App Store if you feel like doing so.
+            Before starting this company, I worked on a variety of web-projects, I interned as a software engineer at Amazon Web Services
+            and RBC, and I also launched an iOS app called Habit One, which you can download on the App Store if you feel like doing so.
           </div>
         </Col>
         <Col className="AboutSecondCol" xl={6}>
           <div className="VideoParent">
             <iframe
-              width="531"
+              width={window.innerWidth < 531 ? window.innerWidth - 10 : 531}
               height="300"
               src="https://www.youtube-nocookie.com/embed/f-lWdbBqrXk?controls=0"
               title="YouTube video player"
               frameborder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowfullscreen
-            ></iframe>
+            />
           </div>
         </Col>
       </Row>

df7b1198ce1ccbbb3304842ceeb19510f1aaaac2
Jean-Philippe Abadir <jpabadir@gmail.com>
2021-05-24 21:08:42 -0400

    Fix PastWork component

--- src/components/PastWork/PastWork.js
+++ src/components/PastWork/PastWork.js
@@ -9,8 +9,8 @@ function PastWork() {
     <div className="PastWorkMain">
       <div className="Subtitle">Our Work</div>
       <Row>
-        {/* <Col /> */}
-        <Col style={{ padding: '0px 0px 0px 0px', margin: '10px 10px 10px 10px' }}>
+        <Col xl={4} />
+        <Col xl={4} style={{ padding: '0px 0px 0px 0px', border: '10px solid transparent' }}>
           <a href="https://apps.apple.com/us/app/habit-one/id1564317049#?platform=iphone" target="_blank" rel="noreferrer">
             <Card className="WorkCard PastWorkFirst">
               <div className="CardTitle">Habit One</div>
@@ -18,23 +18,7 @@ function PastWork() {
             <div className="Overlay"></div>
           </a>
         </Col>
-        <Col style={{ padding: '0px 0px 0px 0px', margin: '10px 10px 10px 10px' }}>
-          <a href="https://apps.apple.com/us/app/habit-one/id1564317049#?platform=iphone" target="_blank" rel="noreferrer">
-            <Card className="WorkCard PastWorkFirst">
-              <div className="CardTitle">Habit One</div>
-            </Card>
-            <div className="Overlay"></div>
-          </a>
-        </Col>
-        <Col style={{ padding: '0px 0px 0px 0px', margin: '10px 10px 10px 10px' }}>
-          <a href="https://apps.apple.com/us/app/habit-one/id1564317049#?platform=iphone" target="_blank" rel="noreferrer">
-            <Card className="WorkCard PastWorkFirst">
-              <div className="CardTitle">Habit One</div>
-            </Card>
-            <div className="Overlay"></div>
-          </a>
-        </Col>
-        {/* <Col /> */}
+        <Col xl={4} />
       </Row>
     </div>
   );

a7eecea8f530c574f81e389a6ff22d8df333009d
Jean-Philippe Abadir <jpabadir@gmail.com>
2021-05-25 13:06:42 -0400

    Add secure redirect

--- server/app.js
+++ server/app.js
@@ -4,6 +4,14 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const forceSsl = (req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  return next();
+};
+
+app.use(forceSsl);
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(port, () => console.log(`App is live on port ${port}!`));

2f7fae20c23cb899f92aac43c0a7a5fa7e6f5899
Jean-Philippe Abadir <jpabadir@gmail.com>
2021-05-26 16:53:49 -0400

    Fix Home padding

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -4,7 +4,7 @@ import './Home.css';
 function Home() {
   return (
     <div className="HomeMain">
-      <div className="HomeMainTitle">We love tech, and we worry about it so that you don't have to.</div>
+      <div className="HomeMainTitle">We love tech, and we worry about it so you don't have to.</div>
     </div>
   );
 }

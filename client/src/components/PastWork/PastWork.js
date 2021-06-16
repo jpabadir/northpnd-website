d1503f438f3816dc464ca14a7f85b051cae5f485
Jean-Philippe Abadir <jpabadir@gmail.com>
2021-06-16 13:07:37 -0400

    Add Interactable class with shadow and pointer cursor

--- client/src/components/PastWork/PastWork.js
+++ client/src/components/PastWork/PastWork.js
@@ -13,7 +13,7 @@ function PastWork() {
           <Col xl={4} />
           <Col xl={4} style={{ padding: '0px 0px 0px 0px', border: '10px solid transparent' }}>
             <a href="https://apps.apple.com/us/app/habit-one/id1564317049#?platform=iphone" target="_blank" rel="noreferrer">
-              <Card className="WorkCard PastWorkFirst">
+              <Card className="WorkCard PastWorkFirst Interactable">
                 <div className="CardTitle">Habit One</div>
               </Card>
               <div className="Overlay"></div>

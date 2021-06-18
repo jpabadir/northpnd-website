5b3853bee1c908cb737d63a6d8e80a8a4fa9c482
Jean-Philippe Abadir <jpabadir@gmail.com>
2021-06-18 16:10:00 -0400

    Deploy build - Add google maps reviews backend

--- server/app.js
+++ server/app.js
@@ -1,17 +1,43 @@
 const express = require('express');
 const path = require('path');
+const https = require('https');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8020;
 
-const forceSsl = (req, res, next) => {
-  if (req.headers['x-forwarded-proto'] !== 'https') {
-    return res.redirect(['https://', req.get('Host'), req.url].join(''));
-  }
-  return next();
-};
+// Main app
+if (process.env.PORT) {
+  const forceSsl = (req, res, next) => {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(['https://', req.get('Host'), req.url].join(''));
+    }
+    return next();
+  };
 
-app.use(forceSsl);
+  app.use(forceSsl);
+}
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(port, () => console.log(`App is live on port ${port}!`));
+
+// Google Maps Reviews
+app.get('/get-reviews', function (mainReq, mainRes) {
+  const options = {
+    hostname: 'maps.googleapis.com',
+    path: `/maps/api/place/details/json?key=${process.env.API_KEY}&place_id=ChIJHbxYLfU1K4gRemEVnxyALR8`,
+    method: 'GET',
+  };
+
+  const req = https.request(options, (res) => {
+    res.on('data', (data) => {
+      mainRes.write(data);
+    });
+  });
+
+  req.on('error', (error) => {
+    console.error(error);
+  });
+
+  req.end();
+});

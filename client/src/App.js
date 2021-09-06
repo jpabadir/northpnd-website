daa8040c983001df9bbbfb4a5d0eb1201fca1057
Jean-Philippe Abadir <jpabadir@gmail.com>
2021-09-05 21:26:22 -0400

    Lint

--- client/src/App.js
+++ client/src/App.js
@@ -142,15 +142,7 @@ function App() {
       </Navbar>
       <div className="ContentParent">
         <div onClick={closeToggle} style={{ position: 'relative', width: '100%' }}>
-          <video
-            src={homeBackground}
-            className="BackgroundVideo Overlay"
-            type="video/mov"
-            playsInline
-            loop
-            autoPlay
-            muted
-          />
+          <video src={homeBackground} className="BackgroundVideo Overlay" type="video/mov" playsInline loop autoPlay muted />
           <div className="Overlay DarkOverlay" />
           <Fade duration="1200">
             <div style={{ width: '100%', justifyContent: 'center', display: 'flex' }}>

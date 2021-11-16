6c7ecdfdb9237b45414dc8235076b2d8d2d2d89a
Jean-Philippe Abadir <jpabadir@gmail.com>
2021-11-15 22:24:15 -0500

    Deploy build - Migrate to client library

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const fetch = require('node-fetch');
 
 const app = express();
 const port = process.env.PORT || 8020;
@@ -20,10 +19,3 @@ if (process.env.PORT) {
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(port, () => console.log(`App is live on port ${port}!`));
-
-// Google Maps Reviews
-app.get('/get-reviews', function (req, res) {
-  fetch(`https://maps.googleapis.com/maps/api/place/details/json?key=${process.env.API_KEY}&place_id=ChIJHbxYLfU1K4gRemEVnxyALR8`)
-    .then((googleResponse) => googleResponse.json())
-    .then((jsonData) => res.send(jsonData['result']['reviews']));
-});

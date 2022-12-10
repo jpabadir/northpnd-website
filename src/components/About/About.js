9c69578a410d9ca4de0518be959145bf30b65b4b
JP Abadir <jpabadir@gmail.com>
2022-12-10 11:04:16 -0500

    Replace video with photo

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,51 +2,29 @@ import React, { useState, useLayoutEffect } from 'react';
 import './About.css';
 import { Row, Col, Container } from 'react-bootstrap';
 import Fade from 'react-reveal/Fade';
+import jpHeadshot from '../../assets/jp-headshot.png';
 
 function About() {
-  const [videoHeight, setVideoHeight] = useState(300);
-
-  useLayoutEffect(() => {
-    function updateVideoHeight() {
-      setVideoHeight(document.getElementById('myVideo').offsetWidth / 1.78);
-    }
-    window.addEventListener('resize', updateVideoHeight);
-    updateVideoHeight();
-    return () => window.removeEventListener('resize', updateVideoHeight);
-  }, []);
-
   return (
     <div className="MainElementPadding">
       <Container fluid className="AboutContainer">
         <Row className="AboutRow">
-          <Col xl={7}>
+          <Col l={8}>
             <div className="Subtitle">Hi! I'm JP.</div>
             <Fade duration={1200}>
               <div className="Subtitle" style={{ fontSize: '25px' }}>
-                I am the founder of North P&amp;D.
+                I am North P&amp;D's founder and main developer.
               </div>
               <div style={{ fontSize: '20px' }}>
                 <br />
-                After working at Amazon Web Services and RBC as a software engineer intern, I started North P&amp;D with the goal of
+                After working at Amazon Web Services and RBC as a software engineer intern, I started North P&amp;D in May of 2021 with the goal of
                 providing high-quality software development and technology services to both technical and non-technical businesses and
                 individuals.
               </div>
             </Fade>
           </Col>
-          <Col xl={5}>
-            <div className="VideoParent">
-              <iframe
-                width="100%"
-                height={videoHeight}
-                src="https://www.youtube-nocookie.com/embed/9Si2XNaDs48?controls=1"
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                id="myVideo"
-                className="StaticShadow"
-              />
-            </div>
+          <Col l={4} className="d-flex justify-content-center">
+            <img src={jpHeadshot} className="JpHeadshot" />
           </Col>
         </Row>
       </Container>

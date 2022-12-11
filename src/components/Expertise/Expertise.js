a7f099e75e24423b9a46f229e8b373759ab0d36a
JP Abadir <jpabadir@gmail.com>
2022-12-11 14:57:59 -0500

    Move stuff to expertise page, remove books

--- src/components/Expertise/Expertise.js
+++ src/components/Expertise/Expertise.js
@@ -1,13 +1,46 @@
-import React from "react";
-import "./Expertise.css";
-import Books from "../Books/Books"
+import expertiseItems from './expertise-items.json';
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import './Expertise.css';
 
-function Expertise() {
+export default function Expertise() {
   return (
     <div>
-      <Books />
+      <TableContainer>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell sx={{ color: 'inherit', fontSize: 'inherit', fontWeight: 'bold' }}>Author</TableCell>
+              <TableCell sx={{ color: 'inherit', fontSize: 'inherit', fontWeight: 'bold' }}>Title</TableCell>
+              <TableCell sx={{ color: 'inherit', fontSize: 'inherit', fontWeight: 'bold' }}>Date Started</TableCell>
+              <TableCell sx={{ color: 'inherit', fontSize: 'inherit', fontWeight: 'bold' }}>Date Finished</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {expertiseItems.map((row) => (
+              <TableRow key={row.title} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                <TableCell sx={{ color: 'inherit', fontSize: 'inherit' }} label="Title">
+                  {row.title}
+                </TableCell>
+                <TableCell sx={{ color: 'inherit', fontSize: 'inherit' }} label="Author">
+                  {row.author}
+                </TableCell>
+                <TableCell sx={{ color: 'inherit', fontSize: 'inherit' }} label="Date Started">
+                  {row.dateStarted}
+                </TableCell>
+                <TableCell sx={{ color: 'inherit', fontSize: 'inherit' }} label="Date Finished">
+                  {row.dateFinished}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
-
-export default Expertise;

f1ad229bbab10238d16263f3f8952dffdb15828e
North PNorthpndstaffD Staff <northpndstaff@Norths-MacBook-Pro.local>
2022-12-15 13:50:04 -0500

    FAQ section on main page

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -1,16 +1,26 @@
+import React, { useState, useLayoutEffect } from 'react';
 import './Faq.css';
+import { Row, Col, Container } from 'react-bootstrap';
+import Fade from 'react-reveal/Fade';
 
 function Faq() {
-    return (
-        <div>
-            <h1 className='FaqHeader'>FAQ</h1>
-            <p className='FaqQuestion'>Question</p>
-            <p className='Faqresponse'>Response</p>
-            <p className='FaqQuestion'>Question</p>
-            <p className='Faqresponse'>Response</p>
-            <p className='FaqQuestion'>Question</p>
-            <p className='Faqresponse'>Response</p>
+  return (
+    <div className="MainElementPadding">
+      <Container fluid>
+        <div className="TextBlock">
+          <div className="Subtitle">FAQ</div>
+          <Fade duration={1200}>
+            <div style={{ fontSize: '25px', paddingLeft: '50px' }}>Question</div>
+            <div style={{ paddingLeft: '50px' }}>Response</div>
+            <div style={{ fontSize: '25px', paddingLeft: '50px' }}>Question</div>
+            <div style={{ paddingLeft: '50px' }}>Response</div>
+            <div style={{ fontSize: '25px', paddingLeft: '50px' }}>Question</div>
+            <div style={{ paddingLeft: '50px'}}>Response</div>
+          </Fade>
         </div>
-    );
+      </Container>
+    </div>
+  );
 }
+
 export default Faq;

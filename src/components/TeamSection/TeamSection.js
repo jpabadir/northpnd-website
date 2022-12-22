5f2499fe9951ce5d072830c33b476cc5b63efeea
North PNorthpndstaffD Staff <northpndstaff@Norths-MacBook-Pro.local>
2022-12-22 15:07:57 -0500

    Add team section

--- src/components/TeamSection/TeamSection.js
+++ src/components/TeamSection/TeamSection.js
@@ -3,6 +3,8 @@ import './TeamSection.css';
 import { Row, Col, Container } from 'react-bootstrap';
 import Fade from 'react-reveal/Fade';
 import JP_Team from '../../assets/JP_Team.jpg';
+import FT_Team from '../../assets/FT_Team.jpg';
+import KJ_Team from '../../assets/KJ_Team.jpg';
 
 function TeamSection() {
     return (
@@ -17,16 +19,32 @@ function TeamSection() {
                 <div className="TextBlock">
                     <div className="Subtitle">Our Team at North P&D</div>
                     <Fade duration={1200}>
-                        <div>
-                            <img src={JP_Team} className="TeamHeadshots" />
+                        <div className='text-center'>
+                            <li className='list-order'>
+                                <img src={JP_Team} className="TeamHeadshots" />
                                 <div className='NameAndOccupation'>
                                     <div style={{ fontSize: '20px' }}>Jean-Philippe</div>
-                                    <div style={{ fontSize: '15px' }}>Principal Software Engineer</div>
+                                    <div style={{ fontSize: '15px' }}>Software Engineer Intern</div>
                                 </div>
+                            </li>
+                            <li className='list-order'>
+                                <img src={KJ_Team} className="TeamHeadshots" />
+                                <div className='NameAndOccupation'>
+                                    <div style={{ fontSize: '20px' }}>Khallan Jamal</div>
+                                    <div style={{ fontSize: '15px' }}>Software Engineer Intern</div>
+                                </div>
+                            </li>
+                            <li className='list-order'>
+                                <img src={FT_Team} className="TeamHeadshots" />
+                                <div className='NameAndOccupation'>
+                                    <div style={{ fontSize: '20px' }}>Fabien Thich</div>
+                                    <div style={{ fontSize: '15px' }}>Software Engineer Intern</div>
+                                </div>
+                            </li>
                         </div>
                     </Fade>
                 </div>
-            </Container> 
+            </Container>
         </div>
     );
 }

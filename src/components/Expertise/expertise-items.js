aae3f9df4507f3b24d0556ed663383c571cefc8d
JP Abadir <jpabadir@gmail.com>
2022-12-31 18:14:42 +0100

    Update

--- src/components/Expertise/expertise-items.js
+++ src/components/Expertise/expertise-items.js
@@ -1,10 +1,10 @@
 export const items = [
-  { "client": "Proximy", "tech": ["Firestore"], "description": "Secured firestore database by writing security rules", "tags": ["security"], "dates": "Jan 2022 - Ongoing" },
-  { "client": "Proximy", "tech": ["Flutter"], "description": "Built many cross-platform mobile features", "tags": ["mobile dev"], "dates": "Jan 2022 - Ongoing" },
+  { "client": "Proximy", "tech": ["Flutter"], "description": "Built many cross-platform mobile features", "tags": ["mobile dev"], "dates": "May 2021 - December 2021" },
+  { "client": "Proximy", "tech": ["Firestore"], "description": "Secured firestore database by writing security rules", "tags": ["security"], "dates": "June 2021" },
   { "client": "Midstride Technologies", "tech": ["PHP", "Laravel", "SQL", "Vue", "JS"], "description": "Built many full-stack features", "tags": ["full-stack dev"], "dates": "Jan 2022 - Ongoing" },
-  { "client": "Midstride Technologies", "tech": ["PHP", "Laravel", "SQL", "Vue", "JS"], "description": "Identified and fixed a stored XSS vulnerability", "tags": ["security", "full-stack dev"], "dates": "Jan 2022 - Ongoing" },
-  { "client": "Midstride Technologies", "tech": ["GitHub Actions"], "description": "Built a CI/CD pipeline to allow Midstride to test and lint its code before any PR gets merged", "tags": ["devops", "CI/CD"], "dates": "Jan 2022 - Ongoing" },
-  { "client": "Midstride Technologies", "tech": ["Bash"], "description": "Wrote a script to facilitate the detection of broken links in hundreds of blog articles", "tags": ["automation"], "dates": "Jan 2022 - Ongoing" }
+  { "client": "Midstride Technologies", "tech": ["GitHub Actions"], "description": "Built and maintained a CI/CD pipeline to allow Midstride to test and lint its code before any PR gets merged", "tags": ["devops", "CI/CD"], "dates": "February 2022 - Ongoing" },
+  { "client": "Midstride Technologies", "tech": ["PHP", "Laravel", "SQL", "Vue", "JS"], "description": "Identified and fixed a stored XSS vulnerability", "tags": ["security", "full-stack dev"], "dates": "August 2022" },
+  { "client": "Midstride Technologies", "tech": ["Bash"], "description": "Wrote a script to facilitate the detection of broken links in hundreds of blog articles", "tags": ["automation"], "dates": "September 2022" }
 ]
 
 export const tagColors = { "security": "red", "full-stack dev": "darkorange", "mobile dev": "blue", "devops": "black", "CI/CD": "green", "automation": "deeppink" }

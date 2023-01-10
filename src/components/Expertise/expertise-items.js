1198bfb7745e2c225cf8035138bdf264a899d40d
JP Abadir <jpabadir@gmail.com>
2023-01-10 10:38:17 -0500

    Update

--- src/components/Expertise/expertise-items.js
+++ src/components/Expertise/expertise-items.js
@@ -3,7 +3,7 @@ export const items = [
   { "client": "Proximy", "tech": ["Cloud Firestore"], "description": "Secured firestore database by writing security rules.", "tags": ["security", "NoSQL"], "dates": "June 2021" },
   { "client": "Midstride Technologies", "tech": ["PHP", "Laravel", "MySQL", "Vue", "JS"], "description": "Built many full-stack web features.", "tags": ["full-stack dev"], "dates": "Jan 2022 - Ongoing" },
   { "client": "Midstride Technologies", "tech": ["GitHub Actions"], "description": "Built and maintained a CI/CD pipeline allowing to test and lint code before any PR gets merged.", "tags": ["devops", "CI/CD", "automation"], "dates": "February 2022 - Ongoing" },
-  { "client": "Midstride Technologies", "tech": ["PHP", "Laravel", "MySQL", "Vue", "JS"], "description": "Spontanenously identified and fixed a stored XSS vulnerability.", "tags": ["security", "full-stack dev"], "dates": "August 2022" },
+  { "client": "Midstride Technologies", "tech": ["PHP", "Laravel", "MySQL", "Vue", "JS"], "description": "Identified and fixed a stored XSS vulnerability.", "tags": ["security", "full-stack dev"], "dates": "August 2022" },
   { "client": "Midstride Technologies", "tech": ["Bash"], "description": "Wrote a script to facilitate the detection of broken links in hundreds of blog articles, avoiding many hours of manual checking.", "tags": ["automation"], "dates": "September 2022" }
 ]
 

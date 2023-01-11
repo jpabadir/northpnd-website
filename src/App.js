b85a7e26d26230e4885026e813cb35de2d4fcfae
FabienThich <fabienthich30@gmail.com>
2023-01-11 14:57:34 -0500

    Understand old system

--- src/App.js
+++ src/App.js
@@ -11,9 +11,16 @@ import Blog from "./components/Blog/Blog";
 import blogsData from "./blogs/blogs-headers.json";
 import { linkify } from "./helpers";
 
+// OLD Scroll System
+// Pass the updateScrollPath function as a prop to LocalNavbar
+// In the LocalNavbar: OnClick will call the updateScrollPath function 
+// ^ This will lowercase the nav item name (in white text) and remove spaces and set that as the path or new value for scrollGoal
+// Main.js has a useEffect that will listen to changes in scrollGoal and then use a prop to scroll to that new scrollGoal 
+
 function App() {
   const [scrollGoal, setScrollGoal] = useState("");
   function updateScrollPath(path) {
+
     setScrollGoal(path);
   }
 

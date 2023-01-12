ecc1a6631d6daaae0d373c8c4ef0b11b54ae3b48
North PNorthpndstaffD Staff <northpndstaff@Norths-MacBook-Pro.local>
2023-01-12 15:56:10 -0500

    Changinf FAQ format

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -6,20 +6,46 @@ import Fade from 'react-reveal/Fade';
 function Faq() {
   return (
     <div className="MainElementPadding">
-      <Container fluid>
-        <div className="TextBlock">
-          <div className="Subtitle">FAQ</div>
-          <Fade duration={1200}>
-            <div style={{ fontSize: '25px', paddingLeft: '50px' }}>Question</div>
-            <div style={{ paddingLeft: '50px' }}>Response</div>
-            <div style={{ fontSize: '25px', paddingLeft: '50px' }}>Question</div>
-            <div style={{ paddingLeft: '50px' }}>Response</div>
-            <div style={{ fontSize: '25px', paddingLeft: '50px' }}>Question</div>
-            <div style={{ paddingLeft: '50px'}}>Response</div>
-          </Fade>
+      <div>
+        <div class="row justify-content-between row mb-4">
+          <div class="col-3" className='Subtitle'>
+            Frequantly asked Questions
+          </div>
+          <div class="col-3">
+            Question 1
+          </div>
         </div>
-      </Container>
-    </div>
+        <div class="row justify-content-end">
+          <div class="col-3">
+            Question 2
+          </div>
+        </div>
+        <div class="row justify-content-end">
+          <div class="col-3">
+            Question 3
+          </div>
+        </div>
+      </div>
+      {/* <Container fluid>
+        <div className="TextBlock">
+          <Row>
+            <Col xl={6}>
+              <div className="Subtitle-FAQ"></div>
+              <Fade duration={1200}>
+                <Col md={6}>
+                  <div className='Questions'>Question</div>
+                  <div className='Response'>Response</div>
+                  <div className='Questions'>Question</div>
+                  <div className='Response'>Response</div>
+                  <div className='Questions'>Question</div>
+                  <div className='Response'>Response</div>
+                </Col>
+              </Fade>
+            </Col>
+          </Row>
+        </div>
+      </Container> */}
+    </div >
   );
 }
 

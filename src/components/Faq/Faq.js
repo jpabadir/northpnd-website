83051888ddcb975d21a254915a653f00b19ece58
North PNorthpndstaffD Staff <northpndstaff@Norths-MacBook-Pro.local>
2023-01-13 15:44:52 -0500

    FAQ redone

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -19,47 +19,6 @@ function Faq() {
             </Col>
           </Row>
       </div>
-
-
-      {/* <div>
-        <div class="row justify-content-between row mb-4">
-          <div class="col-3" className='Subtitle'>
-            Frequantly asked Questions
-          </div>
-          <div class="col-3">
-            Question 1
-          </div>
-        </div>
-        <div class="row justify-content-end">
-          <div class="col-3">
-            Question 2
-          </div>
-        </div>
-        <div class="row justify-content-end">
-          <div class="col-3">
-            Question 3
-          </div>
-        </div>
-      </div> */}
-      {/* <Container fluid>
-        <div className="TextBlock">
-          <Row>
-            <Row xl={6}>
-              <div className="Subtitle-FAQ"></div>
-              <Fade duration={1200}>
-                <Row>
-                  <div className='Questions'>Question</div>
-                  <div className='Response'>Response</div>
-                  <div className='Questions'>Question</div>
-                  <div className='Response'>Response</div>
-                  <div className='Questions'>Question</div>
-                  <div className='Response'>Response</div>
-                </Row>
-              </Fade>
-            </Row>
-          </Row>
-        </div>
-      </Container> */}
     </div >
   );
 }

0cc05384f7b9e67cade1b50c9ac6e5f8f92f57d1
JP Abadir <jpabadir@gmail.com>
2023-01-14 18:01:31 -0500

    Update

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,17 +10,17 @@ function About() {
         <Row className="AboutRow">
           <Col l={8}>
             <div className="Subtitle">Hi! I'm JP.</div>
-              <div className="Subtitle" style={{ fontSize: '25px' }}>
-                I am North P&amp;D's founder and main developer.
-              </div>
-              <div style={{ fontSize: '20px' }}>
-                <br />
-                After working at Amazon Web Services and RBC as a software engineer intern, I started North P&amp;D in May of 2021 with the goal of
-                providing high-quality software development and technology services to both technical and non-technical businesses and
-                individuals.
-              </div>
+            <div className="Subtitle" style={{ fontSize: '25px' }}>
+              I am North P&amp;D's founder and main developer.
+            </div>
+            <div style={{ fontSize: '20px' }}>
+              <br />
+              After working at Amazon Web Services and RBC as a software engineer intern, I started North P&amp;D in May of 2021 with the goal of
+              providing high-quality software development and technology services to both technical and non-technical businesses and
+              individuals.
+            </div>
           </Col>
-          <Col l={4} className="d-flex justify-content-center">
+          <Col l={4} className="d-flex justify-content-center JpHeadshotParent">
             <img src={jpHeadshot} className="JpHeadshot" />
           </Col>
         </Row>

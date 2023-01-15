705419d7921a3e29afd90c8903f8ae11d6ce7a0e
JP Abadir <jpabadir@gmail.com>
2023-01-15 17:07:06 -0500

    Deploy

--- src/components/Expertise/expertise-items.js
+++ src/components/Expertise/expertise-items.js
@@ -5,7 +5,7 @@ export const items = [
   { "client": "Midstride Technologies", "tech": ["GitHub Actions"], "description": "Built and maintained a CI/CD pipeline allowing to automatically test and lint code before any PR gets merged.", "tags": ["devops", "CI/CD", "automation"], "dates": "February 2022 - Ongoing" },
   { "client": "Midstride Technologies", "tech": ["PHP", "Laravel", "MySQL", "Vue", "JS"], "description": "Identified and fixed a stored XSS vulnerability.", "tags": ["security", "full-stack web dev"], "dates": "August 2022" },
   { "client": "Midstride Technologies", "tech": ["Bash"], "description": "Wrote a script to facilitate the detection of broken links in hundreds of blog articles, avoiding many hours of manual checking.", "tags": ["automation"], "dates": "September 2022" },
-  { "client": "Internal Project", "tech": ["React"], "description": "Built this website!", "tags": ["full-stack web dev"], "dates": "May 2021 - Ongoing" }
+  { "client": "Internal Project", "tech": ["React", "DNS Configuration"], "description": "Built this website!", "tags": ["full-stack web dev"], "dates": "May 2021 - Ongoing" }
 ]
 
 export const tagColors = { "security": "red", "full-stack web dev": "darkorange", "mobile dev": "blue", "devops": "black", "CI/CD": "green", "automation": "deeppink", "serverless": "grey", "NoSQL": "lightblue" }

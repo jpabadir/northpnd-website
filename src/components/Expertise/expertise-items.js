46b71c1f9d999ee80d232eee43aed2e8d55b2ba5
JP Abadir <jpabadir@gmail.com>
2023-01-18 15:11:49 -0500

    Deploy

--- src/components/Expertise/expertise-items.js
+++ src/components/Expertise/expertise-items.js
@@ -1,11 +1,11 @@
 export const items = [
   { "client": "Proximy", "tech": ["Flutter", "Google Cloud Functions"], "description": "Built many cross-platform mobile app features.", "tags": ["mobile dev", "serverless"], "dates": "May 2021 - December 2021" },
   { "client": "Proximy", "tech": ["Cloud Firestore"], "description": "Secured Cloud Firestore database by writing security rules.", "tags": ["security", "NoSQL"], "dates": "June 2021" },
-  { "client": "Midstride Technologies", "tech": ["PHP", "Laravel", "MySQL", "Vue", "JS"], "description": "Built many full-stack features.", "tags": ["full-stack web dev"], "dates": "Jan 2022 - Ongoing" },
-  { "client": "Midstride Technologies", "tech": ["GitHub Actions"], "description": "Built and maintained a CI/CD pipeline allowing to automatically test and lint code before any PR gets merged.", "tags": ["devops", "CI/CD", "automation"], "dates": "February 2022 - Ongoing" },
+  { "client": "Midstride Technologies", "tech": ["PHP", "Laravel", "MySQL", "Vue", "JS"], "description": "Currently building many full-stack features.", "tags": ["full-stack web dev"], "dates": "Jan 2022 - Ongoing" },
+  { "client": "Midstride Technologies", "tech": ["GitHub Actions"], "description": "Currently building and maintaining a CI/CD pipeline allowing to automatically test and lint code before any PR gets merged.", "tags": ["devops", "CI/CD", "automation"], "dates": "February 2022 - Ongoing" },
   { "client": "Midstride Technologies", "tech": ["PHP", "Laravel", "MySQL", "Vue", "JS"], "description": "Identified and fixed a stored XSS vulnerability.", "tags": ["security", "full-stack web dev"], "dates": "August 2022" },
   { "client": "Midstride Technologies", "tech": ["Bash"], "description": "Wrote a script to facilitate the detection of broken links in hundreds of blog articles, avoiding many hours of manual checking.", "tags": ["automation"], "dates": "September 2022" },
-  { "client": "Internal Project", "tech": ["React", "DNS Configuration"], "description": "Built this website!", "tags": ["full-stack web dev"], "dates": "May 2021 - Ongoing" }
+  { "client": "Internal Project", "tech": ["React", "DNS Configuration"], "description": "Currently building this website!", "tags": ["full-stack web dev"], "dates": "May 2021 - Ongoing" }
 ]
 
 export const tagColors = { "security": "red", "full-stack web dev": "darkorange", "mobile dev": "blue", "devops": "black", "CI/CD": "green", "automation": "deeppink", "serverless": "grey", "NoSQL": "lightblue" }

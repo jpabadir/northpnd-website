346d8ee476b242d06b1a650d47ad2f0d65670e57
KatrinaMart <k.martel@outlook.com>
2023-02-04 11:19:07 -0800

    remove Khallan

--- src/components/TeamSection/TeamSection.js
+++ src/components/TeamSection/TeamSection.js
@@ -1,35 +1,29 @@
-import React from 'react';
-import './TeamSection.css';
-import { Row, Col, Container } from 'react-bootstrap';
-import JP_Team from '../../assets/JP_Team.jpg';
-import FT_Team from '../../assets/FT_Team.jpg';
-import KJ_Team from '../../assets/KJ_Team.jpg';
+import React from "react";
+import "./TeamSection.css";
+import { Row, Col, Container } from "react-bootstrap";
+import JP_Team from "../../assets/JP_Team.jpg";
+import FT_Team from "../../assets/FT_Team.jpg";
 
 function TeamSection() {
-    return (
-        <div className="MainElementPadding">
-            <Container fluid>
-                <div className="Subtitle">Our Team</div>
-                <Row className='text-center'>
-                    <Col className='my-2'>
-                        <img src={JP_Team} className="TeamHeadshots" />
-                        <div style={{ fontSize: '20px' }}>JP</div>
-                        <div>Founder and Tech Lead</div>
-                    </Col>
-                    <Col className='my-2'>
-                        <img src={FT_Team} className="TeamHeadshots" />
-                        <div style={{ fontSize: '20px' }}>Fabien</div>
-                        <div>Software Engineer - Junior</div>
-                    </Col>
-                    <Col className='my-2'>
-                        <img src={KJ_Team} className="TeamHeadshots" />
-                        <div style={{ fontSize: '20px' }}>Khallan</div>
-                        <div>Software Engineer - Intern</div>
-                    </Col>
-                </Row>
-            </Container >
-        </div >
-    );
+  return (
+    <div className='MainElementPadding'>
+      <Container fluid>
+        <div className='Subtitle'>Our Team</div>
+        <Row className='text-center'>
+          <Col className='my-2'>
+            <img src={JP_Team} className='TeamHeadshots' />
+            <div style={{ fontSize: "20px" }}>JP</div>
+            <div>Founder and Tech Lead</div>
+          </Col>
+          <Col className='my-2'>
+            <img src={FT_Team} className='TeamHeadshots' />
+            <div style={{ fontSize: "20px" }}>Fabien</div>
+            <div>Software Engineer - Junior</div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default TeamSection;

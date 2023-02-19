44764f72ce43fad1cb8fc33b49c327bba6ec3f3e
JP Abadir <jpabadir@gmail.com>
2023-02-19 18:52:05 -0500

    Deploy

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,76 +1,31 @@
 import { Form, Button, Row, Col, Container } from 'react-bootstrap';
 import './Contact.css';
+import { FaArrowRight } from "react-icons/fa";
 
 function Contact() {
   return (
     <div className="MainElementPadding ContactMain">
       <Container fluid className="ContactContainer">
-        <Row>
-          <Col xl={6} className="ContactCol">
-            <div className="Subtitle">Contact us. We'll respond within 24 hours.</div>
-              <div>
-                <br />
-                <div style={{ fontSize: '25px' }}>North P&amp;D, Inc.</div>
-                1 Dundas St. W, 25th Floor
-                <br />
-                M5G 1Z3 
-                <br />
-                Toronto, ON
-                <br />
-                Canada
-                <br />
-                <br />
-                hello@northpnd.com
-                <br />
-                +1 647-576-7112
-              </div>
+        <div className="Subtitle">Contact Us</div>
+        <Row className='ContactRow'>
+          <Col xl={6} className="ContactCol ButtonCol">
+            <a href='https://calendly.com/jpabadir' target="_blank" rel="noopener noreferrer">
+              <button className='ConsultationButton'>
+                Book a free consultation &nbsp;
+                <FaArrowRight style={{ transform: 'translateY(-1px)' }} />
+              </button>
+            </a>
           </Col>
-
-          <Col xl={6}>
-              <div className="FormParent StaticShadow">
-                <Form action="https://formspree.io/f/xwkalnaz" method="POST" className="Form">
-                  <Row>
-                    <Col md={6}>
-                      <Form.Group controlId="formBasicName">
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control className="MyFormControl" type="text" placeholder="Nice to meet you!" name="name" required />
-                      </Form.Group>
-                    </Col>
-                    <Col md={6}>
-                      <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email Address</Form.Label>
-                        <Form.Control
-                          className="MyFormControl"
-                          type="email"
-                          placeholder="Where can we reach you?"
-                          name="_replyto"
-                          required
-                        />
-                      </Form.Group>
-                    </Col>
-                  </Row>
-                  <Form.Group controlId="formBasicMessage" className="form-floating">
-                    <Form.Label>Message</Form.Label>
-                    <Form.Control
-                      style={{ resize: 'none' }}
-                      className="MyFormControl"
-                      type="text"
-                      placeholder="How can we help you?"
-                      name="message"
-                      rows="3"
-                      as="textarea"
-                      required
-                    />
-                  </Form.Group>
-                  <Button variant="primary" type="submit" className="SubmitButton">
-                    Send
-                  </Button>
-                </Form>
-              </div>
+          <Col xl={6} className="ContactCol">
+            <div className='ContactInfo'>
+              hello@northpnd.com
+              <br />
+              +1 647-576-7112
+            </div>
           </Col>
         </Row>
       </Container>
-    </div>
+    </div >
   );
 }
 
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,28 +1,63 @@
 import './Footer.css';
+import { Form, Button, Row, Col, Container } from 'react-bootstrap';
 
 function Footer() {
   return (
     <div className="FooterMain">
       <div className="FooterCompanyName">North P&amp;D, Inc.</div>
       <hr className="FooterHr" />
-      <div className="FooterLinks">
-        <a href="https://www.facebook.com/northpnd" target="_blank" rel="noreferrer" className="FooterLink">
-          Facebook
-        </a>
-        <a href="https://www.linkedin.com/company/northpnd" target="_blank" rel="noreferrer" className="FooterLink">
-          LinkedIn
-        </a>
-        <a href="https://twitter.com/northpndinc" target="_blank" rel="noreferrer" className="FooterLink">
-          Twitter
-        </a>
-        <a href="https://goo.gl/maps/QnvFWXVoY7wKMg88A" target="_blank" rel="noreferrer" className="FooterLink">
-          Google
-        </a>
-        <a href="https://www.northpnd.com/privacy" target="_blank" rel="noreferrer" className="FooterLink">
-          Privacy
-        </a>
-      </div>
-    </div>
+      <Container style={{ paddingBottom: '20px' }}>
+        <Row>
+          <Col xl={6} style={{ textAlign: 'left' }}>
+            <br />
+            1 Dundas St. W, 25th Floor
+            <br />
+            M5G 1Z3
+            <br />
+            Toronto, ON
+            <br />
+            Canada
+            <br />
+            <br />
+          </Col>
+          <Col xl={6} style={{ display: 'flex' }}>
+            <div className='FooterLinks'>
+              <Row style={{ padding: '3px 0px' }}>
+                <Col xs={6} style={{ paddingLeft: '0px' }}>
+                  <a href="https://www.facebook.com/northpnd" target="_blank" rel="noreferrer" className="FooterLink">
+                    Facebook
+                  </a>
+                </Col>
+                <Col xs={6} style={{ paddingLeft: '0px' }}>
+                  <a href="https://www.linkedin.com/company/northpnd" target="_blank" rel="noreferrer" className="FooterLink">
+                    LinkedIn
+                  </a>
+                </Col>
+              </Row>
+              <Row style={{ padding: '3px 0px' }}>
+                <Col xs={6} style={{ paddingLeft: '0px' }}>
+                  <a href="https://twitter.com/northpndinc" target="_blank" rel="noreferrer" className="FooterLink">
+                    Twitter
+                  </a>
+                </Col>
+                <Col xs={6} style={{ paddingLeft: '0px' }}>
+                  <a href="https://goo.gl/maps/QnvFWXVoY7wKMg88A" target="_blank" rel="noreferrer" className="FooterLink">
+                    Google
+                  </a>
+                </Col>
+              </Row>
+              <Row style={{ padding: '3px 0px' }}>
+                <Col xs={6} style={{ paddingLeft: '0px' }}>
+                  <a href="https://www.northpnd.com/privacy" target="_blank" rel="noreferrer" className="FooterLink">
+                    Privacy
+                  </a>
+                </Col>
+              </Row>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div >
   );
 }
 

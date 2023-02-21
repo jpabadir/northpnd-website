c87f55dd4a07d3216a2978163d7882faf399b7af
JP Abadir <jpabadir@gmail.com>
2023-02-20 19:24:32 -0500

    Deploy

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,27 +1,48 @@
 import { Form, Button, Row, Col, Container } from 'react-bootstrap';
 import './Contact.css';
 import { FaArrowRight } from "react-icons/fa";
+import { useEffect, useState } from 'react';
 
 function Contact() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+
+    script.src = "https://assets.calendly.com/assets/external/widget.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    function updateIsMobile() {
+      setIsMobile(document.documentElement.clientWidth < 992);
+    }
+    window.addEventListener('resize', updateIsMobile);
+    updateIsMobile();
+
+    return () => {
+      window.removeEventListener('resize', updateIsMobile);
+      document.body.removeChild(script);
+    }
+  }, []);
+
   return (
     <div className="MainElementPadding ContactMain">
       <Container fluid className="ContactContainer">
         <div className="Subtitle">Contact Us</div>
         <Row className='ContactRow'>
-          <Col xl={6} className="ContactCol ButtonCol">
-            <a href='https://calendly.com/jpabadir/north-p-n-d-initial-consultation' target="_blank" rel="noopener noreferrer">
-              <button className='ConsultationButton'>
-                Book a free consultation &nbsp;
-                <FaArrowRight style={{ transform: 'translateY(-1px)' }} />
-              </button>
-            </a>
-          </Col>
           <Col xl={6} className="ContactCol">
-            <div className='ContactInfo'>
-              hello@northpnd.com
-              <br />
-              +1 647-576-7112
+            <div style={{ fontSize: '18px' }}>
+              We're delighted you're interested in working with us! To get in touch, you
+              can either book a free consultation using the form {isMobile ? 'below' : 'on the right'}, email us at hello@northpnd.com, or give us a call at 647-576-7112.
             </div>
+            <br />
+            <div style={{ fontSize: '18px' }}>
+              We look forward to hearing from you!
+            </div>
+          </Col>
+          <Col xl={6} className="FormParent">
+            <div class="calendly-inline-widget ConsultationForm" data-url="https://calendly.com/jpabadir/north-p-n-d-initial-consultation?hide_event_type_details=1&hide_gdpr_banner=1&background_color=212329&text_color=ffffff"></div>
           </Col>
         </Row>
       </Container>

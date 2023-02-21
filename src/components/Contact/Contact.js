491cc620bc009b027f4cb0470cceb46e4e075577
JP Abadir <jpabadir@gmail.com>
2023-02-20 20:42:56 -0500

    Deploy

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -34,7 +34,7 @@ function Contact() {
           <Col xl={6} className="ContactCol">
             <div style={{ fontSize: '18px' }}>
               We're delighted you're interested in working with us! To get in touch, you
-              can either book a free consultation using the form {isMobile ? 'below' : 'on the right'}, email us at hello@northpnd.com, or give us a call at 647-576-7112.
+              can either book a free consultation using the form {isMobile ? 'below' : 'on the right'}, email us at <a href="mailto:hello@northpnd.com">hello@northpnd.com</a>, or give us a call at <a href='tel:6475767112'>647-576-7112</a>.
             </div>
             <br />
             <div style={{ fontSize: '18px' }}>

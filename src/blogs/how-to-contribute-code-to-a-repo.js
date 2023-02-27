f536857056f4be71574a1dfbf8ba0099298a0d80
JP Abadir <jpabadir@gmail.com>
2023-02-26 21:01:33 -0500

    Update

--- src/blogs/how-to-contribute-code-to-a-repo.js
+++ src/blogs/how-to-contribute-code-to-a-repo.js
@@ -4,7 +4,26 @@ export default function Content() {
   return (
     <div>
       <p>
-        If you've pushed code to your own git repos in the past, you probably only had one single branch, called master or main.
+        If you've pushed code to your own repo in the past, you probably only had one single branch, called <code>master</code> or <code>main</code>. But when you contribute to a repo, you'll often have to work with multiple branches. This is because you'll be working on a feature, and you'll want to keep the <code>master</code> branch clean and stable. So you'll create a new branch for your feature, and then when you're done, you'll merge it into the <code>master</code> branch.
+      </p>
+      <p>
+        Here is how you can create a new branch and submit a PR: 
+        First, checkout the <code>master</code> branch: <code>git checkout master</code>
+        Then, pull the latest changes from the <code>master</code> branch: <code>git pull</code>
+        Then, create a new branch for your feature: <code>git checkout -b my-feature</code>
+        You'll want to call this branch something that describes your feature. For example, if you're working on a new feature called "Add a new blog post", you could call your branch <code>add-new-blog-post</code>.
+      </p>
+      <p>
+        Then, here is how you open your PR on GitHub:
+        First, push your branch to GitHub: <code>git push</code>
+        You'll probably be met with an error message that tells you there is no origin for your branch. If so, you'll need to run the command that comes up and is suggested to you by the error message when you do so. 
+        Then, go to the repo on GitHub and click on the <code>Compare & pull request</code> button.
+        Then, click on the <code>Create pull request</code> button.
+        Then, add a title for your PR. The title should be a short description of your feature. For example, if you're working on a new feature called "Add a new blog post", you could call your PR <code>Add new blog post</code>.
+        Then, click on the <code>Create pull request</code> button again. 
+      </p>
+      <p>
+
       </p>
     </div>
   );

245fde258face3df009549f212cae75a9319bf06
Andy <andy.chai.2006.jiil@gmail.com>
2023-02-28 10:16:19 -0500

    Added GitHub account and downloading Git

---
+++ src/blogs/how-to-push-and-pull-from-github.js
@@ -0,0 +1,40 @@
+import "./BlogStyles.css";
+
+export default function Content() {
+  return (
+    <div>
+      <head>
+        <h1>How to Contribute Code With GitHub</h1>
+      </head>
+      <p>
+        When working with others, an organized platform where everyone can collaborate together is key, and that's exactly what GitHub can provide.
+        For people who has never used or seen GitHub before, phrases like "pushing" or "pulling" may seem alien and unfamiliar, but after reading this article, they'll just be another everyday hassle.
+      </p>
+      <p>
+        Before getting to all that however, first, there are some things you must do before you can start your work on GitHub:
+      </p>
+      <headers>
+        <h2>Creating a GitHub Account</h2>
+      </headers>
+        <img class="fit-picture" src="https://miro.medium.com/max/719/0*LqBi2dONH28oTKVX.png"></img>
+      <p>
+        Before you can start working on GitHub, you need a GitHub account to allow you to access the repositories you wish to edit.
+        Creating a GitHub account is not a difficult task however, and you can do so just by going to the <a href="https://github.com" target="_blank"> GitHub Website</a> and clicking "Sign Up".
+        If you are experiencing troubles, you can take a look at this quick, more in depth tutorial <a href="https://learn.microsoft.com/en-us/visualstudio/version-control/git-create-github-account?view=vs-2022" target="_blank"> here</a>.
+      </p>
+      <headers>
+        <h2>Downloading Git</h2>
+      </headers>
+      <img class="fit-picture" src="https://cdn.freebiesupply.com/logos/thumbs/2x/git-logo.png"></img>
+      <p>
+        The next step towards working with GitHub is downloading Git, which is a control system that is usually used for coordinating work among programmers collaboratively.
+        To download git, go to their website <a href="https://git-scm.com/downloads" target="_blank">here</a> and download the latest source release for your system. 
+        For more set up instructions, visit google or <a href="https://github.com/git-guides/install-git" target="_blank">this website</a> if needed.
+        <img class="fit-picture" src="/Users/andyc/OneDrive/Desktop/Images/Downloading_Git.jpg"></img>
+      </p>
+      <headers>
+        <h2>Downloading VSCode</h2>
+      </headers>
+    </div>
+  );
+}

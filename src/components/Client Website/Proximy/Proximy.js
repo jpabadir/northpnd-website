a79e233c5c4a7814e09a7f34eff571a36ed9faf8
Ezra Song <50184401+ezrasong@users.noreply.github.com>
2023-03-01 14:20:01 -0500

    Added client pages

---
+++ src/components/Client Website/Proximy/Proximy.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Proximy.css';
+
+function Proximy() {
+  return (
+    <div className="container">
+      <div className="image-container">
+        <img alt="Proximy logo" />
+      </div>
+      <div className="right">
+        <h2>Proximy</h2>
+        <p>We helped Proximy build their cross-platform mobile app using Flutter, Cloud Firestore, and Google Cloud Functions.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Proximy;
--- src/components/OurClients/OurClients.js
+++ src/components/OurClients/OurClients.js
@@ -3,15 +3,16 @@ import './PastWork.css';
 import { Card } from 'antd';
 import { Row, Col, Container } from 'react-bootstrap';
 import Reviews from '../Reviews/Reviews';
+import { Link } from "react-router-dom";
 
 function WorkCard(props) {
   return (
     <Col className="PastWorkCol" md={4}>
       <div style={{ maxWidth: '350px', width: '100%' }}>
-        <a href={props.link} target="_blank" rel="noreferrer">
+        <Link to={props.link}>
           <Card className={`WorkCard ${props.title.replace(/\s/g, '')}`} />
           <div className="CardTitle">{props.title}</div>
-        </a>
+        </Link>
         <div>{props.subtitle}</div>
       </div>
     </Col>
@@ -23,24 +24,24 @@ function OurClients() {
     <div className="MainElementPadding">
       <Container fluid>
         <div className="Subtitle">Our Clients</div>
-          <Row>
-            <WorkCard
-              link="https://proximy.ca/"
-              title="Proximy"
-              subtitle="We helped Proximy build their cross-platform mobile app using Flutter, Cloud Firestore, and Google Cloud Functions."
-            />
-            <WorkCard
-              link="https://midstride.com"
-              title="Midstride Technologies"
-              subtitle="We work closely with Midstride's team by communicating directly with its clients and getting a thorough understanding of their requirements before building out full-stack features on their products."
-            />
-            <WorkCard
-              link="https://wearetension.com"
-              title="tension consulting"
-              subtitle="Stay tuned for more info on our work with tension!"
-            />
-          </Row>
-          <Reviews />
+        <Row>
+          <WorkCard
+            link="proximy"
+            title="Proximy"
+            subtitle="We helped Proximy build their cross-platform mobile app using Flutter, Cloud Firestore, and Google Cloud Functions."
+          />
+          <WorkCard
+            link="midstride"
+            title="Midstride Technologies"
+            subtitle="We work closely with Midstride's team by communicating directly with its clients and getting a thorough understanding of their requirements before building out full-stack features on their products."
+          />
+          <WorkCard
+            link="tension"
+            title="tension consulting"
+            subtitle="Stay tuned for more info on our work with tension!"
+          />
+        </Row>
+        <Reviews />
       </Container>
     </div>
   );

86daa67dc40e821f4c3d40e99b3041e8ae6775af
JP Abadir <jpabadir@gmail.com>
2023-03-12 17:44:12 -0400

    Update

--- src/components/OurClients/OurClients.js
+++ src/components/OurClients/OurClients.js
@@ -7,9 +7,9 @@ import { Link } from "react-router-dom";
 
 function WorkCard(props) {
   return (
-    <Col className="PastWorkCol" md={4}>
-      <div style={{ maxWidth: '350px', width: '100%' }}>
-        <Link to={props.link}>
+    <Col className="PastWorkCol" md={3}>
+      <div style={{ maxWidth: '250px', width: '100%' }}>
+        <Link to={props.link} target="_blank" rel="noopener noreferrer">
           <Card className={`WorkCard ${props.title.replace(/\s/g, '')}`} />
           <div className="CardTitle">{props.title}</div>
         </Link>
@@ -26,20 +26,25 @@ function OurClients() {
         <div className="Subtitle">Our Clients</div>
         <Row>
           <WorkCard
-            link="proximy"
+            link="clients/proximy"
             title="Proximy"
             subtitle="We helped Proximy build their cross-platform mobile app using Flutter, Cloud Firestore, and Google Cloud Functions."
           />
           <WorkCard
-            link="midstride"
+            link="clients/midstride"
             title="Midstride Technologies"
             subtitle="We work closely with Midstride's team by communicating directly with its clients and getting a thorough understanding of their requirements before building out full-stack features on their products."
           />
           <WorkCard
-            link="tension"
-            title="tension consulting"
-            subtitle="Stay tuned for more info on our work with tension!"
+            link="clients/tension"
+            title="Tension Consulting"
+            subtitle="We help Tension build and deliver outstanding websites."
           />
+          <WorkCard 
+            link="clients/minebright" 
+            title="Minebright" 
+            subtitle="We provide Minebright with flexbile, on demand, premium Quality Assurance services in support of their software development efforts.">
+          </WorkCard>
         </Row>
         <Reviews />
       </Container>

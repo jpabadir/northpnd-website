2aedb2c4b2fb3b2803f5769082ba686de00c54e9
JP Abadir <jpabadir@gmail.com>
2023-03-18 22:05:57 -0400

    Deploy

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -79,7 +79,7 @@ function Services() {
             videoSrc={discussion}
             posterSrc={discussionPoster}
             title="Non-techical businesses"
-            subtitle="We'll deal with your hosting and domain name so you only have one tech bill to pay - ours."
+            subtitle="We help non-technical companies with strategy, design and development of their digital products. We work with our clients to understand their business and their users, and help them build products that are not only functional but also delightful to use."
             subtitleDisplay={subtitleDisplay}
             setSubtitleDisplay={setSubtitleDisplay}
           />

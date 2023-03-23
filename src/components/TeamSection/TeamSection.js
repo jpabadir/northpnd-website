0661c02610fbfc4798458e90e017ed07386b3f48
Lucas Kim <lucaskamadakim@gmail.com>
2023-03-23 09:41:15 -0400

    Rename profile pictures to use full names

--- src/components/TeamSection/TeamSection.js
+++ src/components/TeamSection/TeamSection.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "./TeamSection.css";
 import { Row, Col, Container } from "react-bootstrap";
-import JP_Team from "../../assets/JP_Team.jpg";
-import KM_Team from "../../assets/KM_Team.jpg";
-import CS_Team from "../../assets/CS_Team.jpg";
-import FT_Team from "../../assets/FT_Team.jpg";
-import AB_Team from "../../assets/AB_Team.png";
-import AP_Team from "../../assets/AP_Team.jpg";
-import ES_Team from "../../assets/ES_Team.jpg";
-import WZ_Team from "../../assets/WZ_Team.jpg";
+import jp_abadir from "../../assets/jp-abadir.jpg";
+import katrina_martel from "../../assets/katrina-martel.jpg";
+import claire_song from "../../assets/claire-song.jpg";
+import fabien_thich from "../../assets/fabien-thich.jpg";
+import aseer_baset from "../../assets/aseer-baset.png";
+import ayesha_parvez from "../../assets/ayesha-parvez.jpg";
+import ezra_song from "../../assets/ezra-song.jpg";
+import william_zhou from "../../assets/william-zhou.jpg";
 import AV_Team from "../../assets/AV_Team.png";
-import WKC_Team from "../../assets/WKC_Team.jpg";
-import LK_Team from "../../assets/LK_Team.jpg";
+import wing_chang from "../../assets/wing-chang.jpg";
+import lucas_kim from "../../assets/lucas-kim.jpg";
 
 function TeamSection() {
   return (
@@ -20,22 +20,22 @@ function TeamSection() {
         <div className='Subtitle'>Our Team</div>
         <div className='TeamMembers'>
           <div className='TeamMember'>
-            <img src={JP_Team} className='TeamHeadshots' />
+            <img src={jp_abadir} className='TeamHeadshots' />
             <div className='TeamNames'>JP</div>
             <div>Founder and Tech Lead</div>
           </div>
           <div className='TeamMember'>
-            <img src={CS_Team} className='TeamHeadshots' id='Claire'/>
+            <img src={claire_song} className='TeamHeadshots' id='Claire'/>
             <div className='TeamNames'>Claire</div>
             <div>Software Engineer</div>
           </div>
           <div className='TeamMember'>
-            <img src={KM_Team} className='TeamHeadshots' id='Katrina' />
+            <img src={katrina_martel} className='TeamHeadshots' id='Katrina' />
             <div className='TeamNames'>Katrina</div>
             <div>Software Engineer</div>
           </div>
           <div className='TeamMember'>
-            <img src={FT_Team} className='TeamHeadshots' />
+            <img src={fabien_thich} className='TeamHeadshots' />
             <div className='TeamNames'>Fabien</div>
             <div>Junior Software Engineer</div>
           </div>
@@ -45,32 +45,32 @@ function TeamSection() {
             <div>Software Engineer Intern</div>
           </div>
           <div className='TeamMember'>
-            <img src={AB_Team} className='TeamHeadshots' id='Aseer'/>
+            <img src={aseer_baset} className='TeamHeadshots' id='Aseer'/>
             <div className='TeamNames'>Aseer</div>
             <div>Software Engineer Intern</div>
           </div>
           <div className='TeamMember'>
-            <img src={ES_Team} className='TeamHeadshots' id='Ezra'/>
+            <img src={ezra_song} className='TeamHeadshots' id='Ezra'/>
             <div className='TeamNames'>Ezra</div>
             <div>Software Engineer Intern</div>
           </div>
           <div className='TeamMember'>
-            <img src={WZ_Team} className='TeamHeadshots'/>
+            <img src={william_zhou} className='TeamHeadshots'/>
             <div className='TeamNames'>William</div>
             <div>Software Engineer Intern</div>
           </div>
           <div className='TeamMember'>
-            <img src={WKC_Team} className='TeamHeadshots'/>
+            <img src={wing_chang} className='TeamHeadshots'/>
             <div className='TeamNames'>Wing</div>
             <div>Software Engineer Intern</div>
           </div>
           <div className='TeamMember'>
-            <img src={LK_Team} className='TeamHeadshots'/>
+            <img src={lucas_kim} className='TeamHeadshots'/>
             <div className='TeamNames'>Lucas</div>
             <div>Software Engineer Intern</div>
           </div>
           <div className='TeamMember'>
-            <img src={AP_Team} className='TeamHeadshots' id='Ayesha'/>
+            <img src={ayesha_parvez} className='TeamHeadshots' id='Ayesha'/>
             <div className='TeamNames'>Ayesha</div>
             <div>UI/UX Intern</div>
           </div>

ef16ca440d3cc4dd1b867456e713835c3cc33e1f
PencilKnot <wingchang.ca@gmail.com>
2023-04-03 10:26:50 -0400

    Added alt text to team images

--- src/components/TeamSection/TeamSection.js
+++ src/components/TeamSection/TeamSection.js
@@ -20,57 +20,57 @@ function TeamSection() {
         <div className='Subtitle'>Our Team</div>
         <div className='TeamMembers'>
           <div className='TeamMember'>
-            <img src={JP_Team} className='TeamHeadshots' />
+          <img src={JP_Team} className='TeamHeadshots' alt='Headshot of JP' />
             <div className='TeamNames'>JP</div>
             <div>Founder and Tech Lead</div>
           </div>
           <div className='TeamMember'>
-            <img src={CS_Team} className='TeamHeadshots' id='Claire'/>
+            <img src={CS_Team} className='TeamHeadshots' alt='Headshot of Claire' id='Claire'/>
             <div className='TeamNames'>Claire</div>
             <div>Software Engineer</div>
           </div>
           <div className='TeamMember'>
-            <img src={KM_Team} className='TeamHeadshots' id='Katrina' />
+            <img src={KM_Team} className='TeamHeadshots' alt='Headshot of Katrina' id='Katrina' />
             <div className='TeamNames'>Katrina</div>
             <div>Software Engineer</div>
           </div>
           <div className='TeamMember'>
-            <img src={FT_Team} className='TeamHeadshots' />
+            <img src={FT_Team} className='TeamHeadshots' alt='Headshot of Fabien' />
             <div className='TeamNames'>Fabien</div>
             <div>Junior Software Engineer</div>
           </div>
           <div className='TeamMember'>
-            <img src={AV_Team} className='TeamHeadshots' />
+            <img src={AV_Team} className='TeamHeadshots' alt='Headshot of Andy' />
             <div className='TeamNames'>Andy</div>
             <div>Software Engineer Intern</div>
           </div>
           <div className='TeamMember'>
-            <img src={AB_Team} className='TeamHeadshots' id='Aseer'/>
+            <img src={AB_Team} className='TeamHeadshots' alt='Headshot of Aseer' id='Aseer'/>
             <div className='TeamNames'>Aseer</div>
             <div>Software Engineer Intern</div>
           </div>
           <div className='TeamMember'>
-            <img src={ES_Team} className='TeamHeadshots' id='Ezra'/>
+            <img src={ES_Team} className='TeamHeadshots' alt='Headshot of Erza' id='Ezra'/>
             <div className='TeamNames'>Ezra</div>
             <div>Software Engineer Intern</div>
           </div>
           <div className='TeamMember'>
-            <img src={WZ_Team} className='TeamHeadshots'/>
+            <img src={WZ_Team} className='TeamHeadshots' alt='Headshot of William' />
             <div className='TeamNames'>William</div>
             <div>Software Engineer Intern</div>
           </div>
           <div className='TeamMember'>
-            <img src={WKC_Team} className='TeamHeadshots'/>
+            <img src={WKC_Team} className='TeamHeadshots' alt='Headshot of Wing' />
             <div className='TeamNames'>Wing</div>
             <div>Software Engineer Intern</div>
           </div>
           <div className='TeamMember'>
-            <img src={LK_Team} className='TeamHeadshots'/>
+            <img src={LK_Team} className='TeamHeadshots' alt='Headshot of Lucas' />
             <div className='TeamNames'>Lucas</div>
             <div>Software Engineer Intern</div>
           </div>
           <div className='TeamMember'>
-            <img src={AP_Team} className='TeamHeadshots' id='Ayesha'/>
+            <img src={AP_Team} className='TeamHeadshots' alt='Headshot of Ayesha' id='Ayesha'/>
             <div className='TeamNames'>Ayesha</div>
             <div>UI/UX Intern</div>
           </div>

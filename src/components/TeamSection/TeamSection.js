6f6eff388448bbba553eee88d3d47ab681be3f77
Lucas Kim <lucaskamadakim@gmail.com>
2023-04-11 13:44:14 -0400

    Convert profile pictures to avif
    Keep jpg as fallback images

--- src/components/TeamSection/TeamSection.js
+++ src/components/TeamSection/TeamSection.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./TeamSection.css";
 import { Row, Col, Container } from "react-bootstrap";
-import JP_Team from "../../assets/JP_Team.jpg";
+import JP_Team from "../../assets/JP_Team.avif";
+import JP_Team_jpg from "../../assets/JP_Team.jpg";
 // import KM_Team from "../../assets/KM_Team.jpg";
-import CS_Team from "../../assets/CS_Team.jpg";
+import CS_Team from "../../assets/CS_Team.avif";
+import CS_Team_jpg from "../../assets/CS_Team.jpg"
 // import FT_Team from "../../assets/FT_Team.jpg";
 
 function TeamSection() {
@@ -13,12 +15,18 @@ function TeamSection() {
         <div className='Subtitle'>Our Team</div>
         <div className='TeamMembers'>
           <div className='TeamMember'>
-            <img src={JP_Team} className='TeamHeadshots' />
+            <picture>
+              <source type='image/avif' srcSet={JP_Team} />
+              <img src={JP_Team_jpg} className='TeamHeadshots' />
+            </picture>
             <div className='TeamNames'>JP Abadir</div>
             <div className='TeamRoles'> Founder and Tech Lead</div>
           </div>
           <div className='TeamMember'>
-            <img src={CS_Team} className='TeamHeadshots' id='Claire'/>
+            <picture>
+              <source type='image/avif' srcSet={CS_Team} />
+              <img src={CS_Team_jpg} className='TeamHeadshots' id='Claire'/>
+            </picture>
             <div className='TeamNames'>Claire Song</div>
             <div className='TeamRoles'>Software Engineer</div>
           </div>

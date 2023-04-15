a195dd81c9c417569a5aa7f957a211856769c7d4
JP Abadir <jpabadir@gmail.com>
2023-04-15 15:00:15 -0400

    Deploy

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './About.css';
 import { Row, Col, Container } from 'react-bootstrap';
+import JP_Team from "../../assets/JP_Team.jpg";
 
 function About() {
   return (
@@ -12,20 +13,26 @@ function About() {
             <div className="Subtitle" style={{ fontSize: '25px' }}>
               I am North P&amp;D's founder and technical lead.
             </div>
+            <div className='d-flex justify-content-center pt-4 JPHeadshotMobileParent'>
+              <img src={JP_Team} className='JPHeadshot' />
+            </div>
             <div style={{ fontSize: '20px' }}>
               <br />
-              After completing software engineering internships at Amazon Web Services and RBC, I established North P&D in May 2021 with the objective of delivering exceptional software development and technology services to businesses. 
+              After completing software engineering internships at Amazon Web Services and RBC, I established North P&D in May 2021 with the objective of delivering exceptional software development and technology services to businesses.
               <br />
               <br />
+              {/* Since then, North P&D has grown and we're now a small team. Our entire team is physically located in Toronto. We're a team of passionate, hard-working, and dedicated individuals who are committed to delivering high-quality software solutions to our clients. Our office is located in the heart of downtown Toronto, come visit us! 
+              <br />
+              <br /> */}
               Click <a href="https://github.com/jpabadir" target="_blank" rel="noreferrer">here</a> to view my GitHub profile, <a href="https://www.linkedin.com/in/jean-philippe-abadir-2a931a157/" target="_blank" rel="noreferrer">here</a> to view my LinkedIn profile, and <a href="expertise" target="_blank" rel="noreferrer">here</a> to learn more about our team's expertise.
             </div>
           </Col>
-          <Col lg={6} className="d-flex justify-content-center AboutVideoParent">
-            <iframe className="AboutVideo" src="https://www.youtube.com/embed/otkSYg5zW_Q" frameborder="0"></iframe>
+          <Col lg={6} className="d-flex justify-content-center AboutVideoParent JPHeadshotDesktopParent">
+            <img src={JP_Team} className='JPHeadshot' />
           </Col>
         </Row>
       </Container>
-    </div>
+    </div >
   );
 }
 

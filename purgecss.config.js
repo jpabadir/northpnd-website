bf54f3fa3622f0315050876e2bb0e3fb7d32ce8a
Lucas Kim <lucaskamadakim@gmail.com>
2023-04-18 11:02:28 -0400

    Add Ant Design card to PurgeCSS config

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["docs/index.html", "docs/static/js/*.js", "node_modules/bootstrap/dist/css/bootstrap.min.css"],
+  content: ["docs/index.html", "docs/static/js/*.js", "node_modules/bootstrap/dist/css/bootstrap.min.css", "node_modules/antd/es/card/**/*.css"],
   css: ["docs/static/css/*.css"],
   output: "docs/static/css"
 }

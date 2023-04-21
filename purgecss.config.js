fadf522646600e6c2a2f50a4d72910fd6b7d6c6f
Lucas Kim <lucaskamadakim@gmail.com>
2023-04-21 09:14:48 -0400

    Add specific classes to PurgeCSS config

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  content: ["docs/index.html", "docs/static/js/*.js", "node_modules/bootstrap/dist/css/bootstrap.min.css", "node_modules/antd/es/card/**/*.css"],
+  content: ["docs/index.html", "docs/static/js/*.js", "node_modules/antd/es/card/**/*.css"],
   css: ["docs/static/css/*.css"],
-  output: "docs/static/css"
+  output: "docs/static/css",
+  safelist: {
+    standard: [/^btn/, /^fixed/],
+    deep: [/^spinner/, /^navbar/, /^nav/, /^row/, /^col/, /^container/, /^form/]
+  }
 }

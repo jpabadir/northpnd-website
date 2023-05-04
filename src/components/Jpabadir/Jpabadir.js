1b1e062feaaf2198821b9825f8b82579b86c2e08
JP Abadir <jpabadir@gmail.com>
2023-05-03 20:23:05 -0400

    Update

--- src/components/Jpabadir/Jpabadir.js
+++ src/components/Jpabadir/Jpabadir.js
@@ -29,13 +29,10 @@ function Jpabadir() {
           <Row style={{ maxWidth: '1700px' }} >
             <Col md={{ span: 8, order: 1 }} style={{ order: 2 }}>
               <br />
-              After completing software engineering internships at Amazon Web Services and RBC, I established North P&D in May 2021 with the objective of delivering exceptional software development and technology services to businesses.
-              Since then, North P&D has grown and we are now a team of two passionate and dedicated developers.
+              Following my software engineering internships at Amazon Web Services in Vancouver and RBC in Toronto, I founded North P&D in May 2021 with the mission to provide exceptional software development and technology services to businesses. Since then, I have been privileged to expand our team and collaborate with clients from various industries, consistently delivering high-quality solutions to meet their diverse needs.
               <br />
               <br />
-              After completing software engineering internships at Amazon Web Services and RBC, I established North P&D in May 2021 with the objective of delivering exceptional software development and technology services to businesses.
-              Since then, North P&D has grown and we are now a team of two passionate and dedicated developers.
-              To learn more, you can visit my.
+              I am passionate about building great software and committed to providing the best possible experience to our clients. I am a strong believer in the power of technology to transform businesses and excited to help our clients leverage it to achieve their goals. I am meticulous and communicative, and I strive to ensure that our clients are always satisfied with our work.
               <br />
               <br />
               <a href="/expertise" style={{ display: 'flex', alignItems: 'center', fontWeight: 'bold', color: 'black', marginTop: '10px' }} className='ExpertiseLink'><div>Learn about our team's expertise</div><MdKeyboardArrowRight color="red" className='ms-4' style={{ transform: 'translateY(3px)' }} size='35px' /></a>

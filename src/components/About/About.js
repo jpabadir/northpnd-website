58f6565ee3bab40486b95c123ca61f2b7e3a7a08
JP Abadir <jpabadir@gmail.com>
2023-05-04 14:34:15 -0400

    Update

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -16,7 +16,7 @@ function About() {
             <img src={flat2} alt="People at work" style={{ height: '160px', width: '100%', objectFit: 'cover' }} />
             <div className='AboutSubtitle'>What we do</div>
             <p>
-              Since 2021, we've delivered custom software solutions to clients from a diverse range of industries, including healthcare, social media, tourism, wellness, and mining. Our services include technical advisory, staff augmentation, design, and development, and we're proud to be a bilingual team fluent in English and French. We build all of our clients' software from our Toronto office at 1 Dundas St W. Feel free to drop by!
+              Since 2021, we've delivered custom software solutions to clients from a diverse range of industries, including healthcare, social media, tourism, wellness, and mining. Our services include technical advisory, staff augmentation, design, and development. We build all of our clients' software from our Toronto office at 1 Dundas St W. Feel free to drop by!
             </p>
           </Col>
           <Col md={4} className='AboutCol'>
@@ -24,7 +24,7 @@ function About() {
             <div className='AboutSubtitle'>Who we work with</div>
             <div>
               <p>
-                We pride ourselves on being able to serve a diverse range of clients, including both non-technical and technical teams. We understand that each company has unique needs and we strive to provide tailored solutions to help our clients achieve their goals. We welcome working with businesses of all sizes, from startups to large enterprises. We also work with non-profit organizations and government agencies.
+                We pride ourselves on being able to serve a diverse range of clients, including both non-technical and technical teams. We welcome working with businesses of all sizes, from startups to large enterprises, as well as non-profit organizations and government agencies. We are proud to be a bilingual team fluent in English and French, and equally comfortable serving clients in either language.
               </p>
             </div>
           </Col>

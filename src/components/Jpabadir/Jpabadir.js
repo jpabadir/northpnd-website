2c31c7e44817613078110173e0f9d52f3f07ed31
JP Abadir <jpabadir@gmail.com>
2023-05-04 20:18:21 -0400

    Update

--- src/components/Jpabadir/Jpabadir.js
+++ src/components/Jpabadir/Jpabadir.js
@@ -29,10 +29,14 @@ function Jpabadir() {
           <Row style={{ maxWidth: '1700px' }} >
             <Col md={{ span: 8, order: 1 }} style={{ order: 2 }}>
               <br />
-              Following my software engineering internships at Amazon Web Services in Vancouver and RBC in Toronto, I founded North P&D in May 2021 with the mission to provide exceptional software development and technology services to businesses. Since then, I have been privileged to expand our team and collaborate with clients from various industries, consistently delivering high-quality solutions to meet their diverse needs.
+              Following my software engineering internships at RBC in Toronto and Amazon Web Services in Vancouver, I founded North P&D in May 2021 with the objective of providing exceptional software development and technology services to businesses. Since then, I have been privileged to expand our team and collaborate with clients from various industries, consistently delivering high-quality solutions to meet their diverse needs.
               <br />
               <br />
-              I am passionate about building great software and committed to providing the best possible experience to our clients. I am a strong believer in the power of technology to transform businesses and excited to help our clients leverage it to achieve their goals. I am meticulous and communicative, and I strive to ensure that our clients are always satisfied with our work.
+              I am passionate about building great software and committed to providing the best possible experience to our clients. I am a strong believer in the power of technology to transform businesses and excited to help our clients leverage it to achieve their goals. 
+I am meticulous and communicative, and I strive to ensure that every project I work on is delivered to the highest level of satisfaction.
+              <br />
+              <br />
+              I hold a Bachelor of Computer Science from the University of British Columbia.
               <br />
               <br />
               <a href="/expertise" style={{ display: 'flex', alignItems: 'center', fontWeight: 'bold', color: 'black', marginTop: '10px' }} className='ExpertiseLink'><div>Learn about our team's expertise</div><MdKeyboardArrowRight color="red" className='ms-4' style={{ transform: 'translateY(3px)' }} size='35px' /></a>

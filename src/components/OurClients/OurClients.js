ef773b28edddf6169b3134ede7bbb6ff8d005f19
emily27yee <emily27yee@gmail.com>
2023-07-18 14:12:33 -0400

    Reformated OurClients section with only logos

--- src/components/OurClients/OurClients.js
+++ src/components/OurClients/OurClients.js
@@ -1,48 +1,41 @@
 import React from 'react';
 import './OurClients.css';
-import { Card } from 'antd';
-import { Row, Col, Container } from 'react-bootstrap';
 
-function WorkCard(props) {
-  return (
-    <Col className="PastWorkCol" md={4}>
-      <div style={{ maxWidth: '250px', width: '100%' }}>
-        <a href={`https://${props.link}`} target="_blank" rel="noreferrer">
-          <Card className={`WorkCard ${props.title.replace(/\s/g, '')}`} />
-          <div className="CardTitle">{props.title}</div>
-        </a>
-        <div dangerouslySetInnerHTML={{ __html: props.subtitle }} />
-      </div>
-    </Col>
-  );
-}
+import cfaeatoncentre from '../../assets/cfaeatoncentre-transparent.png';
+import technucomLogo from '../../assets/technucom-logo.png';
+import proximyLogo from '../../assets/proximy-logo.png';
+import midstrideLogo from '../../assets/midstride-logo-transparent.png';
+import minebrightLogo from '../../assets/minebright-logo.png';
+import digitalriseLogo from '../../assets/digitalrise-logo-transparent.png';
+import kittyscoLogo from '../../assets/kittysco-logo-transparent.png';
 
 function OurClients() {
   return (
     <div className="pt-4">
-      <Container fluid>
-        <Row className="justify-content-center">
-          <div className="Subtitle">Our Clients</div>
-          <WorkCard
-            link="chick-fil-a.ca"
-          />
-          <WorkCard
-            link="technucom.com"
-          />
-          <WorkCard
-            link="proximy.ca"
-          />
-          <WorkCard
-            link="midstride.com"
-          />
-          <WorkCard
-            link="minebright.com"
-          />
-          <WorkCard
-            link="digitalrise.be"
-          />
-        </Row>
-      </Container>
+      <div className="Subtitle">Our Clients</div>
+      <div className="LogosContainer">
+        <a href="https://chick-fil-a.ca" target="_blank" rel="noreferrer">
+          <img src={cfaeatoncentre} alt="Chick-fil-A Toronto Eaton Centre" ></img>
+        </a>
+        <a href="https://technucom.com" target="_blank" rel="noreferrer">
+          <img src={technucomLogo} alt="TechNuCom" ></img>
+        </a>
+        <a href="https://proximy.ca" target="_blank" rel="noreferrer">
+          <img src={proximyLogo} alt="Proximy"></img>
+        </a>
+        <a href="https://midstride.com" target="_blank" rel="noreferrer">
+          <img src={midstrideLogo} alt="Midstride" ></img>
+        </a>
+        <a href="https://minebright.com" target="_blank" rel="noreferrer">
+          <img src={minebrightLogo} alt="Minebright" ></img>
+        </a>
+        <a href="https://digitalrise.be" target="_blank" rel="noreferrer">
+          <img src={digitalriseLogo} alt="Digital Rise" ></img>
+        </a>
+        <a href="https://kittys.co" target="_blank" rel="noreferrer">
+          <img src={kittyscoLogo} alt="Digital Rise" ></img>
+        </a>
+      </div>
     </div>
   );
 }

409e29a96e1f273284570033e3492d94a6dea05e
emily27yee <emily27yee@gmail.com>
2023-07-18 15:35:40 -0400

    Client cards put in loop

--- src/components/OurClients/OurClients.js
+++ src/components/OurClients/OurClients.js
@@ -9,32 +9,54 @@ import minebrightLogo from '../../assets/minebright-logo.png';
 import digitalriseLogo from '../../assets/digitalrise-logo-transparent.png';
 import kittyscoLogo from '../../assets/kittysco-logo-transparent.png';
 
+const clients = [ {
+    imgName: cfaeatoncentre, 
+    webLink: "https://chick-fil-a.ca", 
+    alt: "Chick-fil-A Toronto Eaton Centre"
+  },
+  {
+    imgName: technucomLogo, 
+    webLink: "https://technucom.com", 
+    alt: "TechNuCom"
+  },
+  {
+    imgName: proximyLogo, 
+    webLink: "https://technucom.com", 
+    alt: "Proximy"
+  },
+  {
+    imgName: midstrideLogo, 
+    webLink: "https://midstride.com", 
+    alt: "Midstride"
+  },
+  {
+    imgName: minebrightLogo, 
+    webLink: "https://minebright.com", 
+    alt: "Minebright"
+  },
+  {
+    imgName: digitalriseLogo, 
+    webLink: "https://digitalrise.be", 
+    alt: "Digital Rise"
+  },
+  {
+    imgName: kittyscoLogo, 
+    webLink: "https://kittys.co", 
+    alt: "Kittys Co"
+  }
+]
+
+
 function OurClients() {
   return (
     <div className="pt-4">
       <div className="Subtitle">Our Clients</div>
       <div className="LogosContainer">
-        <a href="https://chick-fil-a.ca" target="_blank" rel="noreferrer">
-          <img src={cfaeatoncentre} alt="Chick-fil-A Toronto Eaton Centre" ></img>
-        </a>
-        <a href="https://technucom.com" target="_blank" rel="noreferrer">
-          <img src={technucomLogo} alt="TechNuCom" ></img>
-        </a>
-        <a href="https://proximy.ca" target="_blank" rel="noreferrer">
-          <img src={proximyLogo} alt="Proximy"></img>
-        </a>
-        <a href="https://midstride.com" target="_blank" rel="noreferrer">
-          <img src={midstrideLogo} alt="Midstride" ></img>
-        </a>
-        <a href="https://minebright.com" target="_blank" rel="noreferrer">
-          <img src={minebrightLogo} alt="Minebright" ></img>
-        </a>
-        <a href="https://digitalrise.be" target="_blank" rel="noreferrer">
-          <img src={digitalriseLogo} alt="Digital Rise" ></img>
-        </a>
-        <a href="https://kittys.co" target="_blank" rel="noreferrer">
-          <img src={kittyscoLogo} alt="Digital Rise" ></img>
-        </a>
+        {clients.map((client) => (
+          <a href={client.webLink} target="_blank" rel="noreferrer">
+            <img src={client.imgName} alt={client.alt} ></img>
+          </a>
+        ))}
       </div>
     </div>
   );
